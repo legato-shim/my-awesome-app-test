e1ae876684d8456784ea88163ba66e479c6ae877
legatoshim <legatoshim@gmail.com>
2019-06-12 18:13:45 +0900

    first heroku commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var http=require('http');
-
-function onRequest(request, response) {
-	response.writeHead(200, {'Content-type': 'text/plain'});
-	response.write('Hello World to Heroku');
-	response.end();
-}
-http.createServer(onRequest).listen(8080);
-console.log("Server Initiated");
+// using the http module
+let http = require('http'),
+port = process.env.PORT || process.argv[2] || 8080;
+// create a simple server
+let server = http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('Hello Heroku using NodeJS', 'utf-8');
+	res.send();
+});
+//listen on the port
+server.listen(port, function() {
+	console.log('App up on port: ' + port);
+});
