8bfa9537e0d623545d26aba703290fbbdb76e78c
legatoshim <legatoshim@gmail.com>
2019-06-12 18:20:16 +0900

    first heroku commit fix 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ port = process.env.PORT || process.argv[2] || 8080;
 let server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('Hello Heroku using NodeJS', 'utf-8');
-	res.send();
+	res.end();
 });
 //listen on the port
 server.listen(port, function() {
