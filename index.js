091ee3323844f0aa0bd456e9aa20e79123ac70e9
legatoshim <legatoshim@gmail.com>
2019-06-14 13:40:07 +0900

    add h2

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ port = process.env.PORT || process.argv[2] || 8080;
 // create a simple server
 let server = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Hello Heroku using NodeJS', 'utf-8');
+	res.write('<H1>Hello Heroku using NodeJS</H1>', 'utf-8');
+	res.write('<p/>', 'utf-8');
+	res.write('<H2>======Hello Heroku using NodeJS ===========</H2>', 'utf-8');
 	res.end();
 });
 //listen on the port
