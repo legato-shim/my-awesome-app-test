3e273a500a4e71e1e2cf1d7f697eb7458b997e71
legatoshim <legatoshim@gmail.com>
2019-06-14 17:29:35 +0900

    6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let http = require('http'),
 port = process.env.PORT || process.argv[2] || 8080;
 // create a simple server
 let server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<H1>Hello Heroku using NodeJS</H1>', 'utf-8');
 	res.write('<p/>', 'utf-8');
 	res.write('<H2>======Hello Heroku using NodeJS ===========</H2>', 'utf-8');
